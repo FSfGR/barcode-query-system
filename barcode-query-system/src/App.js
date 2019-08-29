923a39155dcb0262e54264e4243900598687d6ae
Pingfan Xu <pingfan.xu@mail.utoronto.ca>
2019-08-29 14:35:28 -0400

    setup basis for ScanPage, modified app.js for testing purpose

--- barcode-query-system/src/App.js
+++ barcode-query-system/src/App.js
@@ -3,14 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Home from './pages/Home.jsx';
+import Scan from './pages/Scan.jsx';
 
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <Route exact path="/" component={Home}/>
-      </Router> 
+        {/* <Route exact path="/" component={Home}/> */}
+        <Route exact path="/" component={Scan}/>
+        {/* <Route exact path="/scan" component = {Scan}/> */}
+      </Router>
     )
   }
 }
---
+++ barcode-query-system/src/pages/Scan.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+// import NavBar from '../components/Navigation.jsx';
+
+class Scan extends Component {
+
+  render() {
+    return (
+      <div>
+        This is the Scan Page
+      </div>
+    );
+  }
+}
+
+export default Scan;
