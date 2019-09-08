d063271b7c06e74c1b28b1a2cfe0973326d5a251
Pingfan Xu <pingfan.xu@mail.utoronto.ca>
2019-09-08 00:52:28 -0400

    added usable backend for querying MySQL DB on the cloud

--- backend/server.js
+++ backend/server.js
@@ -8,40 +8,56 @@ app.use(bodyParser.json());
 app.use(cors());
 
 const db = mysql.createConnection({
-    host : 'remotemysql.com',
-    user : 'bbLE5zIBKW',
-    password : 'lyhWBXBfGL',
-    database : 'bbLE5zIBKW'
+  host: 'remotemysql.com',
+  user: 'bbLE5zIBKW',
+  password: 'lyhWBXBfGL',
+  database: 'bbLE5zIBKW'
 });
 
 db.connect();
 
-app.get('/data', function(req,res){
-var sql = 'SELECT * FROM ProductDetails';
-db.query(sql, (err, result)=>{
+app.get('/details', function(req,res){
+  let sql = 'SELECT * FROM ProductDetails';
+  db.query(sql, (err, result)=>{
     if(err) throw err;
     console.log("Results fetched from ProductDetails:");
     console.log(result);
     res.send(result);
-});
+  });
 });
 
-app.post('/data', function(req, res){
+app.post('/details', function(req, res){
 	console.log(req.body);
-    var data = {nama:req.body.nama, usia:req.body.usia};
-    var sql = 'INSERT INTO ninja SET ?';
-    db.query(sql, data, (err, result)=>{
+  let data = {
+    Barcode: req.body.Barcode,
+    Name: req.body.Name,
+    RetailPrice: req.body.RetailPrice,
+    WholesalePrice: req.body.WholesalePrice,
+    Amount: req.body.Amount,
+    Status: req.body.Status,
+    Description: req.body.Description,
+    Remarks: req.body.Remarks,
+    ImageLink: req.body.ImageLink
+  };
+  let sql = 'INSERT INTO ProductDetails SET ?';
+  db.query(sql, data, (err, result)=>{
     if(err) throw err;
     console.log(result);
     res.send({
-        status: 'Data sukses diinput!',
-        no: null,
-		nama: req.body.nama,
-		usia: req.body.usia
-	});
-});
+      requestStatus: 'Successful',
+      Barcode: req.body.Barcode,
+      Name: req.body.Name,
+      RetailPrice: req.body.RetailPrice,
+      WholesalePrice: req.body.WholesalePrice,
+      Amount: req.body.Amount,
+      Status: req.body.Status,
+      Description: req.body.Description,
+      Remarks: req.body.Remarks,
+      ImageLink: req.body.ImageLink
+  	});
+  });
 });
 
-app.listen(3306, ()=>{
-    console.log('Server aktif di port 3306')
+app.listen(3306, () => {
+  console.log('Server listens to port 3306');
 });
